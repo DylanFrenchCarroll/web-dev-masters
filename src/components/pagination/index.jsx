64d77167d3d7a4019eb0ad80dadd1a258ed8fbf2
Dylan French Carroll <Dylan.French.Carroll@gmail.com>
2023-04-05 01:08:49 +0100

    Pagination Added for movies

---
+++ src/components/pagination/index.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "../../styles.css";
+
+const Pagination = ({ nPages, currentPage, setCurrentPage }) => {
+  const pageNumbers = [...Array(nPages + 1).keys()].slice(1);
+
+  const nextPage = () => {
+    if (currentPage !== nPages) setCurrentPage(currentPage + 1);
+  };
+  const prevPage = () => {
+    if (currentPage !== 1) setCurrentPage(currentPage - 1);
+  };
+
+  return (
+    <nav className="paginationNav">
+      <ul className="paginationList justify-content-center">
+        <li className="page-item">
+          <a className="paginationButtons" onClick={prevPage} href="#">
+            Previous
+          </a>
+        </li>
+
+        {pageNumbers.map((pgNumber) => (
+          <li
+            key={pgNumber}
+            className={`paginationButtons ${
+              currentPage == pgNumber ? "active" : ""
+            } `}
+          >
+            <a
+              onClick={() => setCurrentPage(pgNumber)}
+              className="paginationButtons"
+              href="#"
+            >
+              {pgNumber}
+            </a>
+          </li>
+        ))}
+
+        <li className="page-item">
+          <a className="paginationButtons" onClick={nextPage} href="#">
+            Next
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
--- src/components/templateMovieListPage/index.jsx
+++ src/components/templateMovieListPage/index.jsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../headerMovieList";
 import Grid from "@mui/material/Grid";
 import MovieList from "../movieList";
+import Pagination from "../pagination";
 
 const styles = {
-  root: { 
+  root: {
     backgroundColor: "#bfbfbf",
-  }
+  },
 };
 
-function MovieListPageTemplate({ movies, title, action }) {
+function App({ movies, title, action }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recordsPerPage] = useState(2);
+
+  const indexOfLastRecord = currentPage * recordsPerPage;
+  const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+  const currentRecords = movies.slice(indexOfFirstRecord, indexOfLastRecord);
+  const nPages = Math.ceil(movies.length / recordsPerPage);
 
   return (
-    <Grid container sx={styles.root}>
-      <Grid item xs={12}>
-        <Header title={title} />
+    <div className="container mt-5">
+      <Grid container sx={styles.root}>
+        <Grid item xs={12}>
+          <Header title={title} />
+        </Grid>
+        <Grid item container spacing={5}>
+          <MovieList action={action} movies={currentRecords} />
+        </Grid>
       </Grid>
-      <Grid item container spacing={5}>
-      <MovieList action={action} movies={movies} />
-      </Grid>
-    </Grid>
+      <Pagination
+        nPages={nPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+    </div>
   );
 }
-export default MovieListPageTemplate;
+
+export default App;
+
+// export default MovieListPageTemplate;
